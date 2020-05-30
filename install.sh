49e5a9389945a41e627c3ce7c56fd4d5cc7d4052
Katsuhiro Mochida <kome.o.kun@gmail.com>
2020-05-30 14:28:08 +0900

    Add .inputrc

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ ln -s ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
 ln -s ${HOME}/.dotfiles/.zsh ${HOME}/.zsh
 ln -s ${HOME}/.dotfiles/.zsh/.zshenv ${HOME}/.zshenv
 ln -s ${HOME}/.dotfiles/.zsh/.zshrc ${HOME}/.zshrc
+ln -s ${HOME}/.dotfiles/.bashrc ${HOME}/.bashrc
+ln -s ${HOME}/.dotfiles/.inputrc ${HOME}/.inputrc
 
 # 設定の反映
 exec $SHELL -l
